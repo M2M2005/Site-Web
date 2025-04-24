25b2236f042d7d59db8124aa5cf85fb39117528c
M2M2005 <bonscyprien4@gmail.com>
2025-04-24 15:37:56 +0200

    feat : skill-circle

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ burgerMenu.addEventListener('click', () => {
     burgerNav.classList.toggle('active');
     burgerMenu.classList.toggle('open');
 });
+
+// CV - Compétences
+document.addEventListener('DOMContentLoaded', function() {
+    const skillCircles = document.querySelectorAll('.skill-circle');
+
+    skillCircles.forEach(circle => {
+        const percent = parseInt(circle.dataset.percent);
+        const maskAngle = percent * 3.6; // Multiplier par 3.6 pour obtenir l'angle en degrés (100% = 360deg)
+        circle.style.setProperty('--mask-angle', `${maskAngle}deg`);
+    });
+});
